74fca14feb78a6158571e7d04c0748d36b2f5c60
pufaqi <32298636+pufaqi@users.noreply.github.com>
2019-02-23 17:08:24 +0800

    Update loning.py

--- loning.py
+++ loning.py
@@ -3,6 +3,5 @@
 #Author:pufaqi
 #loftybay@163.com
 import connect_database
-data="create table nmbs(id int,username int)"
-#data="select * from user "
+data="select * from mmps"
 connect_database.sqlmysql(data)
